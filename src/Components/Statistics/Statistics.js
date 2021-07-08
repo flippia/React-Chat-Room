99aba74fee2e98b290d54a086c261fe29365a08f
flippia <79864573+flippia@users.noreply.github.com>
2021-07-07 22:39:49 -0400

    Create Statistics.js

---
+++ src/Components/Statistics/Statistics.js
@@ -0,0 +1,11 @@
+import "./Statistics.css";
+
+const Statistics = () => {
+    return (  
+        <div className="statistics">
+            <h1>This page shows statistics</h1>
+        </div>
+    );
+}
+ 
+export default Statistics;
