4c50c2aad2af5e8b0a09dc63d26c4075c85143b1
flippia <79864573+flippia@users.noreply.github.com>
2021-07-07 22:37:28 -0400

    Create About.js

---
+++ src/Components/About/About.js
@@ -0,0 +1,11 @@
+import "./About.css";
+
+const About = () => {
+    return ( 
+        <div className="about">
+            <h1>This is an about page</h1>
+        </div>
+    );
+}
+ 
+export default About;
