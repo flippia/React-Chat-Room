50c6e3fe4e0c5d38a9fd10cf809e333a82d13247
flippia <79864573+flippia@users.noreply.github.com>
2021-07-11 22:50:27 -0400

    Update Header.js

--- src/Components/Chatroom/Header/Header.js
+++ src/Components/Chatroom/Header/Header.js
@@ -2,33 +2,27 @@ import "./Header.css";
 import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 
-const Header = ({user, realTimeListener, switchChatRoom}) => {
+const Header = ({userId, realTimeListener, switchChatRoom}) => {
     const [current, setCurrent] = useState('general');
+
+    const handleClick = (e) => {
+        setCurrent(e.target.innerText.toLowerCase());
+        switchChatRoom(e.target.innerText.toLowerCase());
+        realTimeListener(e.target.innerText.toLowerCase());
+    };
 
     return (  
         <div className="header">
             <button 
-            onClick={(e) => {
-                setCurrent(e.target.innerText.toLowerCase());
-                switchChatRoom(e.target.innerText.toLowerCase());
-                realTimeListener(e.target.innerText.toLowerCase());
-            }}            
+            onClick={handleClick}            
             className={`general ${current ==='general' ? "active" : "inactive"}`}>General</button>
             <button 
-            onClick={(e) => {
-                setCurrent(e.target.innerText.toLowerCase());
-                switchChatRoom(e.target.innerText.toLowerCase());
-                realTimeListener(e.target.innerText.toLowerCase());
-            }}
+            onClick={handleClick}
             className={`music ${current ==='music' ? "active" : "inactive"}`}>Music</button>
             <button 
-            onClick={(e) => {
-                setCurrent(e.target.innerText.toLowerCase());
-                switchChatRoom(e.target.innerText.toLowerCase());
-                realTimeListener(e.target.innerText.toLowerCase());
-            }}
+            onClick={handleClick}
             className={`music ${current ==='game' ? "active" : "inactive"}`}>Game</button>
-            <Link to="/switch">{user}</Link>
+            <Link to="/switch">{userId}</Link>
         </div>
     );
 }
