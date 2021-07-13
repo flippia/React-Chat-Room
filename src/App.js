b4b7363ce0be97fc5a104acda335049d647d1318
flippia <79864573+flippia@users.noreply.github.com>
2021-07-12 23:04:29 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Home from './Components/Home/Home';
 import SwitchUser from './Components/Switch/Switch';
 import Statistics from './Components/Statistics/Statistics';
 import About from './Components/About/About';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
+import { projectAuth } from './firebase/config';
 
 function App() {
+
   return (
     <Router>
       <div className="App">
@@ -15,19 +17,19 @@ function App() {
         </div> 
         <div>
           <Switch>
-            <Route exact path="/">
+            <Route exact path="/" >
               <Home />
             </Route>
-            <Route path="/chat">
-              <Chatroom />
-            </Route>
-            <Route path="/switch">
+            <Route exact path="/chat" render={() => (
+              projectAuth.currentUser !== null ? (<Chatroom />) : (<Redirect to="/switch" />)
+            )} />
+            <Route exact path="/switch">
               <SwitchUser />
             </Route> 
-            <Route path="/statistics">
+            <Route exact path="/statistics">
               <Statistics />
             </Route> 
-            <Route path="/about">
+            <Route exact path="/about">
               <About />
             </Route>            
           </Switch>
