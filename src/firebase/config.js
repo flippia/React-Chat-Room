796e4b84664a81e4c57e479ec48f2de822d6913f
flippia <79864573+flippia@users.noreply.github.com>
2021-07-12 23:04:08 -0400

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,14 @@ import 'firebase/auth';
 import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBZM_UeBul0WfHZKlre6qzR0CRDDUFfuS8",
-    authDomain: "real-time-chat-room-for-react.firebaseapp.com",
-    projectId: "real-time-chat-room-for-react",
-    storageBucket: "real-time-chat-room-for-react.appspot.com",
-    messagingSenderId: "344138517444",
-    appId: "1:344138517444:web:90235cf6b4130cef9c7291"
-};
+    apiKey: "AIzaSyAxwF_s_Svw8mEnoBmZ5hixXLkQcHHUb20",
+    authDomain: "react-chat-room-5c68f.firebaseapp.com",
+    projectId: "react-chat-room-5c68f",
+    storageBucket: "react-chat-room-5c68f.appspot.com",
+    messagingSenderId: "965436119856",
+    appId: "1:965436119856:web:58ccfdd2ad7aa2748086f5",
+    measurementId: "G-8X6N1HRYE7"
+  };
 
 // init firebase
 firebase.initializeApp(firebaseConfig)
